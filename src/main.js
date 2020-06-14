5f363235d674e2d77fb39098c2e05e2a84ff0468
KCVO1995 <943514589@qq.com>
2020-06-14 15:56:35 +0800

    修改卡顿的问题‘

--- src/main.js
+++ src/main.js
@@ -1,18 +1,19 @@
-var clock = null;
-var state = 0;
-var speed = 6;
-var flag = false;
+let clock = null;
+let state = 0;
+let speed = 10;
+let currentScore = 0
+
 
 
 window.onload = () => {
-    const startButton = document.querySelector('.start')
-    startButton.onclick = start()
+    const startButton = $('.start')
+    startButton.onclick = start
 }
 
 // 工具封装
 // 根据id来获取元素
-function $(id) {
-    return document.getElementById(id);
+function $(node) {
+    return document.querySelector(node);
 }
 
 // 创建div, className是其类名
@@ -24,18 +25,8 @@ function creatediv(className) {
 
 //点击开始游戏按钮 开始游戏
 function start() {
-    if (!flag) {
-        init();
-    } else {
-        alert('游戏已经开始，无须再次点击！')
-    }
-}
+    $('.start').classList.add('hidden')
 
-/*
- *    初始化 init
- */
-function init() {
-    flag = true;
     for (var i = 0; i < 4; i++) {
         createrow();
     }
@@ -47,18 +38,20 @@ function init() {
     }
 
     // 定时器 每30毫秒调用一次move()
-    clock = window.setInterval(move, 30);
+    clock = window.setInterval(move, 50);
 }
 
 
 // 判断是否点击黑块、白块
 function judge(ev) {
-    if (ev.target.className.indexOf('black') == -1 && ev.target.className.indexOf('cell') !== -1) {
-        ev.target.parentNode.pass1 = 1; //定义属性pass，表示此行row的白块已经被点击
+    if (ev.target.className.indexOf('black') === -1 && ev.target.className.indexOf('cell') !== -1) {
+        ev.target.parentNode.pass1 = 1; //定义属性pass1，表示此行row的白块已经被点击
     }
 
-    if (ev.target.className.indexOf('black') !== -1) {//点击目标元素 类名中包含 black 说明是黑块
+    if (ev.target.className.indexOf('black') !== -1) {
+        //点击目标元素 类名中包含 black 说明是黑块
         ev.target.className = 'cell';
+        // TODO
         ev.target.parentNode.pass = 1; //定义属性pass，表明此行row的黑块已经被点击
         score();
     }
@@ -66,37 +59,25 @@ function judge(ev) {
 
 }
 
-// 判断游戏是否结束
-
-function over() {
-    var rows = con.childNodes;
-    if ((rows.length == 5) && (rows[rows.length - 1].pass !== 1)) {
-        fail();
-    }
-    for (let i = 0; i < rows.length; i++) {
-        if (rows[i].pass1 == 1) {
-            fail();
-        }
-    }
-
-}
 
 // 游戏结束
 function fail() {
     clearInterval(clock);
-    flag = false;
-    confirm('你的最终得分为 ' + parseInt($('score').innerHTML));
-    var con = $('con');
+    confirm(`你的最终得分为${currentScore}`);
+    $('.start').classList.remove('hidden')
+    var con = $('#con');
     con.innerHTML = "";
-    $('score').innerHTML = 0;
-    con.style.top = '-408px';
-
+    $('#score').innerHTML = 0;
+    con.style.top = '0px';
 }
 
 // 创造一个<div class="row">并且有四个子节点<div class="cell">
 function createrow() {
-    var con = $('con');
+    var con = $('#con');
     var row = creatediv('row'); //创建div className=row
+    const time = 75000 / speed
+    row.style.transition = `all ${time}s linear`
+    row.style.height = '150px'
     var arr = creatcell(); //定义div cell的类名,其中一个为cell black
 
     con.appendChild(row); // 添加row为con的子节点
@@ -123,45 +104,61 @@ function creatcell() {
 
 //让黑块动起来
 function move() {
-    var con = $('con');
-    var top = parseInt(window.getComputedStyle(con, null)['top']);
+    let con = $('#con');
+    let top = parseInt(window.getComputedStyle(con, null)['top']);
 
     if (speed + top > 0) {
         top = 0;
     } else {
         top += speed;
     }
+
     con.style.top = top + 'px';//不断移动top值，使它动起来
     over();
-    if (top == 0) {
+    if (top === 0) {
         createrow();
-        con.style.top = '-102px';
+        con.style.top = '-150px';
         delrow();
     }
 }
+
+// 判断游戏是否结束
+function over() {
+    let rows = con.childNodes;
+    if ((rows.length == 5) && (rows[rows.length - 1].pass !== 1)) {
+        fail();
+    }
+    for (let i = 0; i < rows.length; i++) {
+        if (rows[i].pass1 === 1) {
+            fail();
+        }
+    }
+}
+
+
 
 
 // 加速函数
 function speedup() {
     speed += 2;
-    if (speed == 20) {
-        alert('你超神了');
-    }
+    // if (speed === 20) {
+    //     alert('你超神了');
+    // }
 }
 
 //删除某行
 function delrow() {
-    var con = $('con');
-    if (con.childNodes.length == 6) {
+    var con = $('#con');
+    if (con.childNodes.length === 8) {
         con.removeChild(con.lastChild);
     }
 }
 
 // 记分
 function score() {
-    var newscore = parseInt($('score').innerHTML) + 1;//分数加一
-    $('score').innerHTML = newscore;//修改分数
-    if (newscore % 10 == 0) {//当分数是10 的倍数时使用加速函数，越来越快
+    currentScore += 1
+    $('#score').innerHTML = currentScore;//修改分数
+    if (currentScore % 5 === 0) {//当分数是10 的倍数时使用加速函数，越来越快
         speedup();
     }
 }
